79b178b83843da412b41bed5e179772577415617
felixyangyicheng <felix.yang.yicheng@gmail.com>
2023-09-14 15:47:18 +0200

    test

---
+++ Google-Like-Blazor.Test/Services/FileServiceTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Google_Like_Blazor.Data;
+using System.Runtime;
+using Google_Like_Blazor.Services;
+using MongoDB.Driver;
+
+namespace Google_Like_Blazor.Test.Services
+{
+    [TestClass]
+
+    public class FileServiceTest
+	{
+        private IFileRepo _fileRepo;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+ 
+        var mongoClient = new MongoClient("mongodb://root:123456@localhost:27017");
+
+        var mongoDatabase = mongoClient.GetDatabase(
+                "google-like");
+
+        var _collection = mongoDatabase.GetCollection<FileModel>(
+                "files");
+            _fileRepo = new FileService(_collection);
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            
+        }
+
+        [TestMethod]
+
+        public async Task SearchWithKeyNotNull()
+        {
+            // Arrange
+            string key= "sport";
+            // Act
+            var result = await _fileRepo.SearchInContent(key);
+            // Assert
+            Assert.IsNotNull(result);
+    
+        }
+
+
+        [TestMethod]
+
+        public async Task SearchWithWhitespaceReturn0()
+        {
+            // Arrange
+            string key = " ";
+            // Act
+            var result = await _fileRepo.SearchInContent(key);
+            // Assert
+            Assert.IsTrue(result.Count()==7);
+
+        }
+    }
+
+}
+
---
+++ Google-Like-Blazor/Utils/MemoryStorageUtility.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Google_Like_Blazor.Utils
+{
+    public class MemoryStorageUtility
+    {
+        public Dictionary<string, object> Storage { get; set; } = new();
+    }
+}
+
